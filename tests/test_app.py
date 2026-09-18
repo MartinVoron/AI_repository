894adbccefc751216c6ea2cce02cf4045bd08d84
Martin VORON <martin_voron@carrefour.com>
2026-09-18 15:31:51 +0200

    Test CI failure handling

--- tests/test_app.py
+++ tests/test_app.py
@@ -32,6 +32,9 @@ class TestApp(unittest.TestCase):
         with self.assertRaises(ZeroDivisionError):
             divide(5, 0)
 
+    def test_divide_intentional_failure():
+        assert divide(10, 2) == 999
+
 
 if __name__ == "__main__":
     unittest.main()
